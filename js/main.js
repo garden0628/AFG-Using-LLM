cb906cc2841788922302e24df79c005b69b19528
garden0628 <bill990628@gmail.com>
2023-11-13 23:09:51 +0900

    Latest Version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 let editor;
+const description = document.getElementById("description-content");
+const testcaseContainer = document.getElementById("testcase-container");
+const testcaseWidth = testcaseContainer.clientWidth * 0.45;
 
 window.onload = function() {
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/python");
+}
+
+function generateFeedback() {
+    var code = editor.getSession().getValue();
+    var testcases = [];
+
+    console.log(description.value);
+    console.log(code);
+}
+
+function addTestCase() {
+    var input = document.createElement("textarea");
+    var output = document.createElement("textarea");
+
+    input.placeholder = "Input";
+    console.log(testcaseContainer.style.width);
+    input.style.width = `${(testcaseWidth)}px`;
+    input.style.margin = `5px 15px 3px 0`;
+    output.placeholder = "Output";
+    output.style.width = `${(testcaseWidth)}px`;
+    output.style.margin = `5px 15px 3px 0`;
+
+    testcaseContainer.appendChild(input);
+    testcaseContainer.appendChild(output)
 }
